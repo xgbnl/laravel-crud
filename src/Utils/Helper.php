e3451da254476603c718e7720a99c44f3f279ca2
xgbnl <745502274@qq.com>
2022-06-30 12:44:11 +0800

    feta: 新增helper,抽出公共复用方法

---
+++ src/Utils/Helper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Xgbnl\Business\Utils;
+
+class Helper
+{
+    final static public function strEndWith(string $haystack, string|array $needle): string
+    {
+        if (is_string($needle)) {
+            return str_ends_with($haystack, $needle) ? substr($haystack, 0, -strlen($needle)) : $haystack;
+        }
+
+        foreach ($needle as $need) {
+            if (str_ends_with($haystack, $need)) {
+                return substr($haystack, 0, -strlen($need));
+            }
+        }
+
+        return $haystack;
+    }
+}
