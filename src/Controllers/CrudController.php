0f47f85a43e777ff76245d07958a3c41efa5358e
xgbnl <745502274@qq.com>
2022-07-04 10:10:33 +0800

    chore: 优化Crud控制器

--- src/Controllers/CrudController.php
+++ src/Controllers/CrudController.php
@@ -4,11 +4,14 @@ namespace Xgbnl\Business\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Xgbnl\Business\Paginator\Paginator;
+use  Illuminate\Database\Eloquent\Model;
 
 /**
  * @method JsonResponse json(mixed $data = null, int $code = 200)
  * @method Paginator customPaginate(array $list = [], bool $isPaginate = true)
  * @method array filter(array $arrays)
+ * @method array filterFields(array &$origin, array|string $fields)
+ * @method void trigger(int $code, string $message)
  */
 abstract class CrudController extends AbstractController
 {
@@ -27,7 +30,9 @@ abstract class CrudController extends AbstractController
             $id = $this->request->input('id');
         }
 
-        abort_if(empty($id), 422, '无效数据获取，参数ID不能为空');
+        if (empty($id)){
+            $this->trigger(422,'ID不能为空');
+        }
 
         $model = $this->repository->find($id);
 
@@ -38,29 +43,23 @@ abstract class CrudController extends AbstractController
         return $this->json($model);
     }
 
-    final protected function doStoreOrUpdate(array $extras = [], string $by = 'id', mixed $filterFields = null): JsonResponse
+    final protected function doStoreOrUpdate(array $extras = [], string $by = 'id', mixed $filterFields = null, bool $json = true): JsonResponse|Model
     {
         $validated = $this->validatedForm($extras);
 
-        abort_if(empty($validated), 422, '插入数据不能为空');
+        if (empty($validated)){
+            $this->trigger(422,'插入数据不能为空');
+        }
 
         $validated = $this->filter($validated);
 
-        if (is_string($filterFields) && isset($validated[$filterFields])) {
-            unset($validated[$filterFields]);
-        }
-
-        if (is_array($filterFields)) {
-            foreach ($filterFields as $field) {
-                if (isset($validated[$field])) {
-                    unset($validated[$field]);
-                }
-            }
+        if (!is_null($filterFields)){
+            $validated = $this->filterFields($validated, $filterFields);
         }
 
         $model = $this->service->createOrUpdate($validated, by: $by);
 
-        return $this->json($model);
+        return $json ? $this->json($model) : $model;
     }
 
     final protected function doDestroy(?int $id = null): JsonResponse
@@ -76,23 +75,14 @@ abstract class CrudController extends AbstractController
             return $this->json();
         }
 
-        abort_if(empty($this->request->all()), 422, '无效的删除');
+        if (empty($this->request->all())) {
+            $this->trigger(422, '无效的删除');
+        }
 
         $ids = $this->filter($this->request->all());
 
         $this->service->destroy($ids);
 
         return $this->json();
-    }
-
-    final protected function getKey(string $by = 'id', string $method = 'input'): mixed
-    {
-        if (!in_array($method, ['input', 'get'])) {
-            abort(500, '调用方式只能为input或get');
-        }
-
-        $id = $this->request->{$method}($by);
-
-        return !empty($id) ? $id : abort(422, 'id不能为空');
     }
 }
